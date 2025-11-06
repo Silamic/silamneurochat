9fb9d90709d3d9957a91a1294db11dc2b7dac318
Silamic <Silamulelasihle3@gmail.com>
2025-11-06 10:44:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Project root (where index.html is)
-  build: {
-    outDir: 'dist', // Output folder for production build
-  },
   server: {
-    port: 5173, // local dev server port
-  },
+    proxy: {
+      "/api": "http://localhost:3000"
+    }
+  }
 });
